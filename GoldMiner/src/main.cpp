f49ff253d189ad12cc87b6a1c1c18fe1984b7ac7
smdrr <1498646901@qq.com>
2024-07-04 14:37:36 +0800

    第一次 commit，大致框架

---
+++ GoldMiner/src/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <QApplication>
+#include <QWidget>
+#include "src/app.h"
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+    QWidget window;
+    // demo：产生一个空白的窗口
+    window.resize(250, 150);
+    window.setWindowTitle("Simple Qt Application");
+    window.show();
+
+    return app.exec();
+}
